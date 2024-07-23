90046ea3cbe7e3837c9ae13863de27b05cc2839d
shalev396 <shalev396@gmail.com>
2024-07-23 15:27:52 +0300

    commit

---
+++ 2024/07_July/23/HomeWork/main.js
@@ -0,0 +1,11 @@
+console.log("hi");
+let firstName="Shalev";
+let LastName="Ben Moshe";
+let age=21;
+let IsStudent =true;
+
+if(age>18)
+    {
+
+    }
+console.log(str1)
--- 2024/07_July/23/main.js
+++
@@ -1,3 +0,0 @@
-console.log("hi");
-let str="abc"
-let cha="a"
