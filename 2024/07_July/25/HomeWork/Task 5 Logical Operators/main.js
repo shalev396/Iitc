2f5d1c35875428a82e4f722bb395f8d5509817a4
shalev396 <shalev396@gmail.com>
2024-07-25 19:42:26 +0300

    proggres on HomeWork

---
+++ 2024/07_July/25/HomeWork/Task 5 Logical Operators/main.js
@@ -0,0 +1,17 @@
+//chapter E Arithmetic Operations
+//1. && || !
+//2. return true only if both condition are true
+//3. true
+//4. false
+//5. return true if at least one of the condition are met
+//6. true
+//7. false
+//8. it returns true only if the condition are not met
+//9. False
+//10. true
+//11. false
+//12. true
+//13.
+//14.
+//// 15. yes, he dose not (if a is false)
+////16. no, he dose
