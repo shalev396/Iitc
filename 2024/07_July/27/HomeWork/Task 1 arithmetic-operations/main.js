0bbf101fb329c47a7935f851a061897eaf464618
shalev396 <shalev396@gmail.com>
2024-07-28 08:59:34 +0300

    added 27/07/2024 Fixes

---
+++ 2024/07_July/27/HomeWork/Task 1 arithmetic-operations/main.js
@@ -0,0 +1,21 @@
+//chapter A Arithmetic Operations
+//1. 8
+//2. use + in between valuables
+//3. "-"
+//4. 6
+//5. use * between values
+//6. 12
+//7. "/"
+//8. 5
+//9. Infinity
+//10. use % in between values
+//11. 2
+//12. x++
+//13. They do the same but one it short
+//14. y--
+//15. 20
+//16. use of "()"" like this 10+(5*2)
+//17. 30
+//18. Number("5")
+//19. 53
+//20.5
