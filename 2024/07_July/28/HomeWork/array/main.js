18741b01f57099f3ced2aa14e042fbfffec7485a
shalev396 <shalev396@gmail.com>
2024-07-28 16:28:38 +0300

    added HomeWork for 28/07/2024 (added27/07/2024 broken folder)

---
+++ 2024/07_July/28/HomeWork/array/main.js
@@ -0,0 +1,24 @@
+//Array
+//1.
+let fruits = [];
+//2.
+fruits.push("apple");
+fruits.push("banana");
+fruits.push("mango");
+fruits.push("blueberry");
+//3.
+lastItem = fruits.pop();
+//4.
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers[2]); //third element is in index 2
+//5.
+fruits[2] = "kiwi";
+//6.
+let colors = [];
+colors.push("red");
+colors.push("green");
+colors.push("blue");
+//7.
+colors.shift();
+//8.
+colors.unshift("yellow");
