ad77bf37b451c45f7a454656f4c022200f77ad52
shalev396 <shalev396@gmail.com>
2024-08-16 23:16:03 +0300

    finished 15/8/2024 HomeWork

--- 2024/08_August/15/HomeWork/SQLNorthWindHomeWorkChapter5Query.sql
+++ 2024/08_August/15/HomeWork/SQLNorthWindHomeWorkChapter5Query.sql
@@ -1,4 +1,4 @@
---chapter 5 subqury
+--chapter 5 SubQuery
 --task 1
 SELECT ProductName, UnitPrice
 FROM Products
@@ -58,5 +58,26 @@ select OrderID,OrderDate
 from Orders
 where ShipCountry=(select ShipCountry
 					from Orders
-					where ShipCountry='germany' or ShipCountry='france' or ShipCountry='sweeden')
+					where ShipCountry='germany' or ShipCountry='france' or ShipCountry='Sweden')
 					and YEAR(MAX(Orders.OrderDate))=1997
+--task 11
+select ProductName,ProductID
+from Products
+where UnitPrice>(select avg(UnitPrice)
+				 from Products
+				 where UnitsInStock>50)
+--task 12
+select ProductName, CategoryName,Region
+from Products
+join Categories on Products.CategoryID=Categories.CategoryID
+join Suppliers on Suppliers.SupplierID=Products.SupplierID
+where CategoryName in (select CategoryName
+					from Categories
+					where CategoryName IN ('Beverages', 'Condiments')) 
+ AND Region IS NULL;
+--task 13
+select distinct CompanyName
+from Suppliers
+join Products on Products.SupplierID=Suppliers.SupplierID
+join Categories on Categories.CategoryID=Products.CategoryID
+where CategoryName='Beverages'
