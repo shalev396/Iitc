090e399797917c0cd68c67fd882d7a6c81fb604d
shalev396 <shalev396@gmail.com>
2024-09-05 12:58:25 +0300

    added "add new product"

--- Projects/SQL/TeamProjectApiTesting/server.js
+++ Projects/SQL/TeamProjectApiTesting/server.js
@@ -6,11 +6,12 @@ require("dotenv").config();
 const app = express();
 const port = 3000;
 
-// Route to get data
-app.get("/task1/:add", async (req, res) => {
+//use this template
+///http://localhost:3000/VPD/true?prodId=3&userId=4
+app.get("/VPD/:add", async (req, res) => {
   try {
-    const prodId = 1;
-    const userId = 1;
+    const prodId = req.query.prodId || 1; // default to 1 if not provided
+    const userId = req.query.userId || 1; // default to 1 if not provided
     const add = req.params.add;
     const response = await axios.get(
       `https://fakestoreapi.com/products/${prodId}`
@@ -60,8 +61,34 @@ app.get("/task1/:add", async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
-app.get("/task1A", async (req, res) => {
+
+//use this us a template
+//http://localhost:3000/ANP?title=Test%20Title&price=100&description=Great%20Product&image=https://i.pravatar.cc&category=electronics
+app.get("/ANP", async (req, res) => {
   try {
+    const title = req.query.title || "default title"; // 'default title' is used if title is not provided
+    const price = req.query.price || "0"; // default price of 0
+    const description = req.query.description || "default description";
+    const image = req.query.image || "https://i.pravatar.cc"; // Default image URL
+    const category = req.query.category || "default category";
+
+    let headers = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+    const data = {
+      title: title,
+      price: price,
+      description: description,
+      image: image,
+      category: category,
+    };
+    const response = await axios.post(
+      `https://fakestoreapi.com/products`,
+      data,
+      headers
+    );
+    res.json(response.data);
   } catch (err) {
     console.error("server error", err);
     res.status(500).send("Server Error");
