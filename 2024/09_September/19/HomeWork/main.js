43b93c5d99fbfdc31bbbb4496bcc6eb5ee18c3e0
shalev396 <shalev396@gmail.com>
2024-09-19 17:41:17 +0300

    progress on 19/9/2024 HomeWork

---
+++ 2024/09_September/19/HomeWork/main.js
@@ -0,0 +1,14 @@
+"use strict";
+// List of available products with names and prices
+const products = [
+  { id: 1, name: "Laptop", price: 1000 },
+  { id: 2, name: "Smartphone", price: 600 },
+  { id: 3, name: "Headphones", price: 100 },
+  { id: 4, name: "Keyboard", price: 50 },
+  { id: 5, name: "Mouse", price: 30 },
+  { id: 6, name: "Monitor", price: 300 },
+  { id: 7, name: "Printer", price: 150 },
+  { id: 8, name: "Webcam", price: 80 },
+  { id: 9, name: "USB Cable", price: 10 },
+  { id: 10, name: "External Hard Drive", price: 120 },
+];
