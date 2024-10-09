fd519631aaf692b553580508c6420a74b2414764
shalev396 <shalev396@gmail.com>
2024-10-09 17:04:40 +0300

    finished 09/10/2024 HomeWork

--- 2024/10_October/08/HomeWork/view.js
+++ 2024/10_October/08/HomeWork/view.js
@@ -8,10 +8,15 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 //https://bible-api.com/john 3:16
 const img = document.getElementById("img");
 const p = document.getElementById("p");
-fetch(`https://cataas.com/cat`)
+const p1 = document.getElementById("p1");
+const p2 = document.getElementById("p2");
+const p3 = document.getElementById("p3");
+fetch(`https://picsum.photos/id/0/info`)
   .then((response) => response.json())
   .then(async (data) => {
-    img.src = data;
+    console.log(data);
+
+    img.src = data.download_url;
     console.log("img");
   })
   .catch((error) => console.error("Error fetching array:", error));
@@ -20,5 +25,29 @@ fetch(`https://bible-api.com/john%203:16`)
   .then(async (data) => {
     p.textContent = data.text;
     console.log("p");
+  })
+  .catch((error) => console.error("Error fetching array:", error));
+
+fetch(`https://fakestoreapi.com/products/1`)
+  .then((response) => response.json())
+  .then(async (data) => {
+    p1.textContent = data.title;
+    console.log("p1");
+  })
+  .catch((error) => console.error("Error fetching array:", error));
+
+fetch(`https://api.adviceslip.com/advice`)
+  .then((response) => response.json())
+  .then(async (data) => {
+    p2.textContent = data.slip.advice;
+    console.log("p2");
+  })
+  .catch((error) => console.error("Error fetching array:", error));
+
+fetch(`http://api.citybik.es/v2/networks`)
+  .then((response) => response.json())
+  .then(async (data) => {
+    p3.textContent = data.networks[0].name;
+    console.log("p3");
   })
   .catch((error) => console.error("Error fetching array:", error));
