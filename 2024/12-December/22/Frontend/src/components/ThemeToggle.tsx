9e4711b7678024f36015469536db0e8c01c1c5f4
shalev396 <shalev396@gmail.com>
2024-12-23 10:58:07 +0200

    23/12/2024 HomeWork

---
+++ 2024/12-December/22/Frontend/src/components/ThemeToggle.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "./ThemeProvider";
+
+export function ThemeToggle() {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <Button variant="outline" size="icon" onClick={toggleTheme}>
+      {theme === "light" ? (
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem]" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
